7d504c57dbaa713e21c260af699b176473b4bfa5
Colleen <cdnickerson05@gmail.com>
2023-10-18 22:07:05 -0300

    Added TSF as CMake library.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,21 @@
+#define TSF_IMPLEMENTATION
+#define TML_IMPLEMENTATION
+#include <tsf.h>
+#include <tml.h>
+
 #include <iostream>
 #include <SiliconLucid.hpp>
 
 int main() {
+    tsf* TinySoundFont = tsf_load_filename("soundfont.sf2");
+    tsf_set_output(TinySoundFont, TSF_MONO, 44100, 0); //sample rate
+    tsf_note_on(TinySoundFont, 0, 60, 1.0f); //preset 0, middle C
+    short HalfSecond[22050]; //synthesize 0.5 seconds
+    tsf_render_short(TinySoundFont, HalfSecond, 22050, 0);
+    tsf_close(TinySoundFont);
+
     InitWindow(320 * 4, 240 * 4, "SILICON LUCID");
+    InitAudioDevice();
 
     while(!WindowShouldClose()) {
         BeginDrawing();
@@ -10,6 +23,7 @@ int main() {
         EndDrawing();
     }
 
+    CloseAudioDevice();
     CloseWindow();
 
     return 0;
