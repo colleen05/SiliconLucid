98bb4159ba6dc7178ef7ce2b34b35ae1702b73de
Colleen <cdnickerson05@gmail.com>
2023-10-18 22:31:41 -0300

    soundfont included

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,21 +7,40 @@
 #include <SiliconLucid.hpp>
 
 int main() {
-    tsf* TinySoundFont = tsf_load_filename("soundfont.sf2");
+    tsf* TinySoundFont = tsf_load_filename("./resources/soundfonts/a320_neo.sf2");
     tsf_set_output(TinySoundFont, TSF_MONO, 44100, 0); //sample rate
-    tsf_note_on(TinySoundFont, 0, 60, 1.0f); //preset 0, middle C
-    short HalfSecond[22050]; //synthesize 0.5 seconds
-    tsf_render_short(TinySoundFont, HalfSecond, 22050, 0);
+    tsf_note_on(TinySoundFont, 96, 60, 1.0f); //preset 0, middle C
+    short RenderedSound[44100*2]; //synthesize 1.0 seconds
+    tsf_render_short(TinySoundFont, RenderedSound, 44100*2, 0);
     tsf_close(TinySoundFont);
 
     InitWindow(320 * 4, 240 * 4, "SILICON LUCID");
     InitAudioDevice();
 
+    Wave wav = {
+        44100*2,
+        44100,
+        16,
+        1,
+        &RenderedSound
+    };
+
+    Sound snd = LoadSoundFromWave(wav);
+
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(C_DKMAGENTA);
+
+        if(IsKeyPressed(KEY_SPACE)) {
+            SetSoundPitch(snd, 0.25f + ((float)GetRandomValue(0, 100) / 100.0f));
+            PlaySound(snd);
+        }
+
         EndDrawing();
     }
+
+    UnloadSound(snd);
+    UnloadWave(wav);
 
     CloseAudioDevice();
     CloseWindow();
